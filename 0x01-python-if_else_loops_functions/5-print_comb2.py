21526beb9e7fd8352ccf9ce9e8ff29d26ddf73b6
ellalearns <ellalearnscopy@gmail.com>
2023-08-10 18:35:52 +0000

    make exe, solve syntax error

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i == 99:
-        print(f"{i}")
+        print(i)
     elif i < 10:
         print("{}".format("0" + str(i)), end=", ")
     else:
