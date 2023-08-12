b70373ec0b0809f62b21fea8b070982be3aa89ca
ellalearns <ellalearnscopy@gmail.com>
2023-08-12 04:20:57 +0000

    make exe plus some changes

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,6 +5,6 @@ a = 10
 b = 5
 
 print("{} + {} = {}".format(a, b, add(a, b)), end="\n")
-print("{} + {} = {}".format(a, b, add(a, b)), end="\n")
-print("{} + {} = {}".format(a, b, add(a, b)), end="\n")
-print("{} + {} = {}".format(a, b, add(a, b)), end="\n")
+print("{} - {} = {}".format(a, b, sub(a, b)), end="\n")
+print("{} * {} = {}".format(a, b, mul(a, b)), end="\n")
+print("{} / {} = {}".format(a, b, div(a, b)), end="\n")
