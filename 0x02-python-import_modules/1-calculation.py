502f395fe1920250fbbf98bad5e98f2294b2867c
Ella <ellalearnscopy@gmail.com>
2023-08-12 05:37:11 +0100

    continuation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    """run add, sub, mul, and div from calculator_1.py"""
+    from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
+    a = 10
+    b = 5
 
-print("{} + {} = {}".format(a, b, add(a, b)), end="\n")
-print("{} - {} = {}".format(a, b, sub(a, b)), end="\n")
-print("{} * {} = {}".format(a, b, mul(a, b)), end="\n")
-print("{} / {} = {}".format(a, b, div(a, b)), end="\n")
+    print("{} + {} = {}".format(a, b, add(a, b)), end="\n")
+    print("{} - {} = {}".format(a, b, sub(a, b)), end="\n")
+    print("{} * {} = {}".format(a, b, mul(a, b)), end="\n")
+    print("{} / {} = {}".format(a, b, div(a, b)), end="\n")
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    """print all arguments used to run program"""
+    from sys import argv
 
-args = len(argv)
+    args = len(argv)
 
-if args == 1:
-    print("{} arguments.".format(args - 1))
-elif args == 2:
-    print("{} argument:".format(args - 1))
-else:
-    print("{} arguments:".format(args - 1))
+    if args == 1:
+        print("{} arguments.".format(args - 1))
+    elif args == 2:
+        print("{} argument:".format(args - 1))
+    else:
+        print("{} arguments:".format(args - 1))
 
-if args >= 2:
-    arg = 1
-    while arg < args:
-        print("{}: {}".format(arg, argv[arg]))
-        arg += 1
+    if args >= 2:
+        arg = 1
+        while arg < args:
+            print("{}: {}".format(arg, argv[arg]))
+            arg += 1
