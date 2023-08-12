05fad25f2aec95cf373a0a7fc5d97cb8153b9732
Ella <ellalearnscopy@gmail.com>
2023-08-12 06:00:44 +0100

    continuation

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    import builtins
     """print builtins"""
-    dir()
+    dir(builtins)
+    print("{}".format("my_secret_santa"))
+    print("{}".format("print_hidden"))
+    print("{}".format("print_school"))
