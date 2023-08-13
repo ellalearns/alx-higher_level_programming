63b87bc1d4fba9127213dc32ac64fadb1b475628
Ella <ellalearnscopy@gmail.com>
2023-08-13 23:27:46 +0100

    task 0 continue

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    """print all integers of a list"""
+def print_list_integer(my_list=[]):
+    """Function to print integers in list.
+    Assuming all entries are integers.
 
-    def print_list_integer(my_list=[]):
-        """Function to print integers in list.
-        Assuming all entries are integers.
+    Args:
+        my_list: the list of integers
 
-        Args:
-            my_list: the list of integers
+    Returns:
+        void
 
-        Returns:
-            void
-
-        """
-        for int in my_list:
-            print("{}".format(int))
+    """
+    for int in my_list:
+        print("{:d}".format(int))
