956d17ecc6a0f289da353356e888cc83c9b3483b
Ella <ellalearnscopy@gmail.com>
2023-08-14 00:57:21 +0100

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    """print the integers in a matrix.
+    Assuming all the elements in the matrix are integers.
+
+    Args:
+        matrix: the matrix list
+
+    Returns:
+        null
+    """
+    for matrium in matrix:
+        for elem in matrium:
+            if matrium.index(elem) == len(matrium) - 1:
+                print("{:d}".format(elem), end="\n")
+            else:
+                print("{:d} ".format(elem), end="")
