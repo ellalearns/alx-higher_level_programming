a0a972a8d54e87b43aa75e929d44c88f17c60632
Ella <ellalearnscopy@gmail.com>
2023-08-17 05:54:05 +0100

    amend 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,7 +4,7 @@ def multiply_by_2(a_dictionary):
     """multiply all values in a dict by 2
     returns a new dict
     """
-    new_dict = a_dictionary
-    for key in a_dictionary:
+    new_dict = a_dictionary.copy()
+    for key in new_dict:
         new_dict[key] *= 2
     return new_dict
