0911f8aed5f149651327ba112b0b947c14bff6a3
Ella <ellalearnscopy@gmail.com>
2023-08-17 04:10:13 +0100

    task 9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    """multiply all values in a dict by 2
+    returns a new dict
+    """
+    new_dict = a_dictionary
+    for key in new_dict:
+        new_dict[key] *= 2
+    return new_dict
