79ddcd755b281004641f665ec3f71c60dffadae4
Ella <ellalearnscopy@gmail.com>
2023-08-17 03:26:19 +0100

    task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    """adds all unique ints in a list
+
+    Args:
+        my_list: the list of integers
+
+    Returns:
+        int: sum of all unique ints in the list
+    """
+    new_list = my_list
+    mto = []
+    added = []
+    total = 0
+
+    for x in new_list:
+        if new_list.count(x) > 1:
+            if x not in mto:
+                mto.append(x)
+
+    for x in new_list:
+        if x in mto:
+            if x not in added:
+                total += x
+                added.append(x)
+        else:
+            total += x
+
+    return new_list, mto, added, total
