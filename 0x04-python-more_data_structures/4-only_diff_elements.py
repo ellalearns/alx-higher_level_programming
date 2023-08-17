2b346f54a7f6d726c3cc0f396a4392a7472f0abb
Ella <ellalearnscopy@gmail.com>
2023-08-17 03:38:48 +0100

    task 4

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+def only_diff_elements(set_1, set_2):
+    """set of elements present in only one set
+    Args:
+        set_1: first set
+        set_2: second set
+    Returns:
+        list: elements in one set
+    """
+    return set_1 ^ set_2
