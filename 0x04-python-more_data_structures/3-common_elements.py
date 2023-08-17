d8030eea85a0f22a092fe6df98fb3d77bf573c70
Ella <ellalearnscopy@gmail.com>
2023-08-17 03:32:20 +0100

    task 3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """return common elements in two sets
+    Args:
+        set_1: first set
+        set_2: second set
+    Returns:
+        set: contains elements in both arg sets
+    """
+    return set_1 & set_2
