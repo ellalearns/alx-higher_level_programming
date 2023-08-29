0028a2075dd6f5404e97f7479a63a7759277915a
Ella <ellalearnscopy@gmail.com>
2023-08-29 05:14:34 +0100

    tasks 5 and 6

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    """
+    raises a type exception
+    :return: none
+    """
+    raise TypeError()
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    """
+    raises a name exception with a message
+    :param message: the message
+    :return: none
+    """
+    raise NameError(message)
