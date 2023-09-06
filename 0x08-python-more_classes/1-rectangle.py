a9f6796911c7214d26c2d3d82db172bb6d711d78
Ella <ellalearnscopy@gmail.com>
2023-09-06 20:48:11 +0100

    task 1 amends

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle:
         :param width: optional, defaults to 0
         :param height: optional, defaults to 0
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
