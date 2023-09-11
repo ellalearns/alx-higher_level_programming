ed67382ad42570b37827e7f087d3d410bb207a6c
Ella <ellalearnscopy@gmail.com>
2023-09-12 00:15:51 +0100

    feat: task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -12,7 +12,8 @@ def inherits_from(obj, a_class):
     :return true or false
     """
     if issubclass(type(obj), a_class):
-        return True
+        if type(obj) is not a_class:
+            return True
 
     else:
         return False
