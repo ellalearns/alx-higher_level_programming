667984d3c785a1dac77f61900e911157e84da8ab
Ella <ellalearnscopy@gmail.com>
2023-09-12 00:39:00 +0100

    feat: task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+rectangle class that inherits from BaseGeometry
+"""
+BG = __import__('7-base_geometry.py').BaseGeometry
+
+
+class Rectangle(BG):
+    """
+    rectangle subclass  class
+    """
+    def __init__(self, width, height):
+        if BG.integer_validator(width):
+            self.__width = width
+
+        if BG.integer_validator(height):
+            self.__height = height
