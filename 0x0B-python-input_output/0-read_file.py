1fb9058d22a9fcf95e13b3bbb8285b3ade0aab32
Ella <ellalearnscopy@gmail.com>
2023-09-12 21:25:37 +0100

    feat: read a file (basic) (task 0)

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def read_file(filename=""):
+    """
+    read a file
+    :param filename: the name of file
+    :return: nothing
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        data_read = file.read()
