9639c4f3331b9d6ebdf9b5781039bf7723042295
Ella <ellalearnscopy@gmail.com>
2023-09-12 21:56:04 +0100

    feat: load json from file (task 6) -- refactor

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -9,7 +9,9 @@ def load_from_json_file(filename):
     """
     load json obj from txt file
     :param filename: the file to load from
-    :return: nothing
+    :return: the object data
     """
-    with open(filename, "w", encoding="utf-8") as file:
-        json.load(file)
+    with open(filename, "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    return data
