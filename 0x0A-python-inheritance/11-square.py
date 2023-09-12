e25412252d9119527dac2993ea242214e60fae9e
Ella <ellalearnscopy@gmail.com>
2023-09-12 01:07:52 +0100

    feat: task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+square inherits from rectangle
+"""
+Rect = __import__('9-rectangle').Rectangle
+
+
+class Square(Rect):
+    """
+    another doc
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
