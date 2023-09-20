187a8a65675fbe96c587f660b825b217220cf266
ellalearns <ellalearnscopy@gmail.com>
2023-09-20 11:22:32 +0100

    Task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+a function that adds integers
+"""
+
+def add_integer(a, b=98):
+    """
+    adds two integers
+
+    param a: first integer
+    param b: second integer
+
+    Return: an integer
+    """
+    if not isinstance(a, int):
+        if not isinstance(a, float):
+            raise TypeError("a must be an integer")
+
+    if not isinstance(b, int):
+        if not isinstance(b, float):
+            raise TypeError("b must be an integer")
+
+
+    if isinstance(a, float):
+        a = int(a)
+
+    if isinstance(b, float):
+        b = int(b)
+
+
+    return int(a + b)
+
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+base README for Test Driven Development project
