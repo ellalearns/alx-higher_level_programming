058c0590177f88647df1df89744976be90723a7d
Ella <ellalearnscopy@gmail.com>
2023-10-02 22:03:53 +0100

    feat

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -8,11 +8,18 @@ from models.rectangle import Rectangle
 
 class TestRectangle(unittest.TestCase):
     def setUp(self):
+        """
+        more docs inu fdr dfgr go
+        """
         self.rect1 = Rectangle(4, 6)
         self.rect2 = Rectangle(2, 1)
         self.rect3 = Rectangle(1, 3, 1, 4, 5)
 
     def width(self):
+        """
+        you want docs? chop them.
+        :return:
+        """
         self.assertEqual(self.rect1.width, 4)
         self.assertEqual(self.rect2.width, 2)
         self.assertEqual(self.rect3.width, 1)
