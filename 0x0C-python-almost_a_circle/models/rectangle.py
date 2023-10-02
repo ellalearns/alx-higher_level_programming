4493748336e0e6957498f5f0e78c8049bc84a6b7
Ella <ellalearnscopy@gmail.com>
2023-10-02 22:05:26 +0100

    feat

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,11 +7,11 @@ from base import Base
 
 class Rectangle(Base):
     """
-    rect from Base
+    rect from Base. more words ooo. chop them oo.
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         """
-        doc, you hear
+        doc, you hear. more words ooo. chop them oo.
         """
         super().__init__(id)
         self.__width = width
@@ -22,55 +22,55 @@ class Rectangle(Base):
     @property
     def width(self):
         """
-        private getter
+        private getter. more words ooo. chop them oo.
         """
         return self.__width
 
     @width.setter
     def width(self, value):
         """
-        prop okay?
+        prop okay?. more words ooo. chop them oo.
         """
         self.__width = value
 
     @property
     def height(self):
         """
-        private getter
+        private getter. more words ooo. chop them oo.
         """
         return self.__height
 
     @height.setter
     def height(self, value):
         """
-        prop okay?
+        prop okay?. more words ooo. chop them oo.
         """
         self.__height = value
 
     @property
     def x(self):
         """
-        private getter
+        private getter. more words ooo. chop them oo.
         """
         return self.__x
 
     @x.setter
     def x(self, value):
         """
-        prop okay?
+        prop okay? more words ooo. chop them oo.
         """
         self.__x = value
 
     @property
     def y(self):
         """
-        private getter
+        private getter. more words ooo. chop them oo.
         """
         return self.__y
 
     @y.setter
     def y(self, value):
         """
-        prop okay?
+        prop okay? more words ooo. chop them oo.
         """
         self.__width = value
