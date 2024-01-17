931b244d86f41beee44736ddb43206e63acbadcf
ellalearns <ellalearnscopy@gmail.com>
2024-01-17 20:08:00 +0100

    task 13

---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    """
+    replicates a python bytecode process
+    """
+    const = 98
+    b_power = a ** b
+    result = const + b_power
+    return result
