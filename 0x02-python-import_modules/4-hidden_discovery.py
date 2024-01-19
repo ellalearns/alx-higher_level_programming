3080c7159320e91aac373ff5ba8e710722cf1865
ellalearns <ellalearnscopy@gmail.com>
2024-01-19 05:43:07 +0100

    task 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+if __name__ == "__main__":
+    """prints from hidden pyc"""
+    import "hidden_4"
+
+    files = dir(hidden_4)
+    for file in files:
+        if file[0:2] != "__":
+            print(file)
