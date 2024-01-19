a43df72faf24a407a0d4e0e863c069d6595542d0
ellalearns <ellalearnscopy@gmail.com>
2024-01-19 05:24:11 +0100

    mods

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """
-    imports funcs from calculator_1 py
-    add, sub, mul, div
-    """
+    """imports funcs from calculator_1 py"""
     from calculator_1 import add, sub, mul, div
 
     a = 10
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -10,12 +10,11 @@ if __name__ == "__main__":
     total = 0
 
     if len(argv) > 1:
-        infinite_number = argv[1]
         total = 0
-        index = 0
+        index = 1
 
-        while (index < len(infinite_number)):
-            total += int(str(infinite_number[index]))
+        while (index < len(argv)):
+            total += int(argv[index])
             index += 1
     
     print("{}".format(total))
