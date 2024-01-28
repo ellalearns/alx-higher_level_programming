d0827b7fa1b4ff6736e3cf11a54db0b9f0f6be58
Ella <ellalearnscopy@gmail.com>
2024-01-28 08:39:07 +0100

    task 0

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """
+    prints the integers in a list,
+    assuming all list elements are integers
+    """
+    for inte in my_list:
+        print("{}".format(inte))
