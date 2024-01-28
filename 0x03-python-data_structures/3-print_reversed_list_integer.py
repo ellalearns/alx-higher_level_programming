f8697f2c99bea6341cefea3bbcaa1a9946fd5f99
Ella <ellalearnscopy@gmail.com>
2024-01-28 09:01:04 +0100

    mods

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,9 +5,9 @@ def print_reversed_list_integer(my_list=[]):
     print the elements of a list
     in reversed order
     """
-    if my_list == None:
+    if my_list is None:
         my_list = []
-    
+
     counter = 0
     rev_counter = -1
     while counter in range(len(my_list)):
