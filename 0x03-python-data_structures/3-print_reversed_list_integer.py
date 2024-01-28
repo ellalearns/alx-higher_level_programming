06e7fb975a4d421b66e75e33921f0a08602afa65
Ella <ellalearnscopy@gmail.com>
2024-01-28 08:57:45 +0100

    task 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    """
+    print the elements of a list
+    in reversed order
+    """
+    counter = 0
+    rev_counter = -1
+    while counter in range(len(my_list)):
+        print("{:d}".format(my_list[rev_counter]))
+        rev_counter -= 1
+        counter += 1
