94ed52440458b55845d5911dc7ac9b45b3221a89
Ella <ellalearnscopy@gmail.com>
2024-01-28 09:00:01 +0100

    mods

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -14,5 +14,3 @@ def print_reversed_list_integer(my_list=[]):
         print("{:d}".format(my_list[rev_counter]))
         rev_counter -= 1
         counter += 1
-
-print_reversed_list_integer(None)
