253a7d461264ee8c95f1f52e334c6cd926d210d4
Ella <ellalearnscopy@gmail.com>
2024-01-28 10:05:55 +0100

    mods

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,14 +5,14 @@ def print_matrix_integer(matrix=[[]]):
     print the integers of a matrix
     """
     if matrix is None:
-        print("", end="$\n")
+        print("", end="\n")
     if matrix == [[]]:
-        print("", end="$\n")
+        print("", end="\n")
     for listd in matrix:
         counter = 0
         while counter < len(listd):
             if (counter == len(listd) - 1):
-                print("{:d}".format(listd[counter]), end="$\n")
+                print("{:d}".format(listd[counter]), end="\n")
             else:
                 print("{:d}".format(listd[counter]), end=" ")
             counter += 1
