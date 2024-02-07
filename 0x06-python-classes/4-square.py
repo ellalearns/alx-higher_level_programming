a119517345c2302fc3db143f8e04e25707d02b81
Ella <ellalearnscopy@gmail.com>
2024-02-08 00:14:41 +0100

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+contains an empty class square
+"""
+
+
+class Square:
+    """
+    my new square class
+    """
+    def __init__(self, size=0):
+        if (isinstance(size, int)):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def size(self):
+        """
+        retrieve size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if (isinstance(value, int)):
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """
+        returns area of a square
+        """
+        return self.__size * self.__size
