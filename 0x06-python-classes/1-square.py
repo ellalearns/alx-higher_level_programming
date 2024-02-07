19e2ed0c56865bb2d691955e43d5b97cfa429d21
Ella <ellalearnscopy@gmail.com>
2024-02-07 23:59:22 +0100

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+contains an empty class square
+"""
+
+
+class Square:
+    """
+    my new square class
+    """
+    def __init__(self, size):
+        self.__size = size
