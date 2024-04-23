42b6a859d6d57da2f54ef7c89a05ee6592446bef
ellalearns <ellalearnscopy@gmail.com>
2024-04-23 15:51:34 +0100

    task 1 mods

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const firstLine = 'C is fun';
 const secondLine = 'Python is cool';
-const thirdLine = 'JavaScript is amazin';
-console.log(firstLine + '\n');
-console.log(secondLine + '\n');
-console.log(thirdLine + '\n');
+const thirdLine = 'JavaScript is amazing';
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
