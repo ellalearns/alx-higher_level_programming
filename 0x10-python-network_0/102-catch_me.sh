9ee744e7a32d083438e75875f442938d6e383287
ellalearns <ellalearnscopy@gmail.com>
2024-05-10 05:45:44 +0100

    task 9 mods

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # curl request that returns a message containaing a required string and displays body of response
-curl -s -X PUT "0.0.0.0:5000/catch_me" -H "Origin: HolbertonSchool" -d "user_id=98" -L
+curl -s "0.0.0.0:5000/catch_me" -H "Origin: HolbertonSchool" -L
