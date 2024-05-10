150f70c54e13e6d9a516a63b7165b96a2b0897ca
ellalearns <ellalearnscopy@gmail.com>
2024-05-10 04:53:18 +0100

    task 8 mods

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends curl post json request with data from file and displays response body
-curl -s -X POST -H "Content-Type: application/json" -d $(cat $2) $1 -L
+curl -s -X POST -H "Content-Type: application/json" -d "$(cat $2)" $1 -L
