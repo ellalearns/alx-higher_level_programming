eea660b77e0ba3eb1a74745a3dd84cddc0f4acd0
ellalearns <ellalearnscopy@gmail.com>
2024-05-10 04:31:31 +0100

    task 5

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# sends a curl post request and displays the response
+curl -s -X POST $1 \
+-H "Content-Type: multipart/form-data" \
+-F "email=test@gmail.com" -F "subject=I will always be here for PLD" \
+-L
