fe55b338f8ba60289f901a87abbdaccca589866d
ellalearns <ellalearnscopy@gmail.com>
2024-06-12 03:28:40 +0100

    standard

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,17 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   const newElem = '<li>Item</li>';
   const domToMod = 'UL.my_list';
 
   $('DIV#add_item').on('click', function () {
     $(domToMod).append(newElem);
-  })
+  });
 
   $('DIV#remove_item').on('click', function () {
     $(domToMod).children(':last-child').remove();
-  })
+  });
 
   $('DIV#clear_list').on('click', function () {
     $(domToMod).empty();
-  })
-  
+  });
+
 });
