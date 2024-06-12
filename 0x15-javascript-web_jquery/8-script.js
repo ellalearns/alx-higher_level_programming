a567972863c0941df7fbd2c081a31db4fc26dff6
ellalearns <ellalearnscopy@gmail.com>
2024-06-12 02:19:30 +0100

    task 8

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+$.ajax({
+    url: "https://swapi-api.alx-tools.com/api/films/?format=json",
+    type: "GET",
+    success: function (data) {
+        const titleNum = data.results.length;
+        for (counter = 0; counter < titleNum; counter++) {
+            const title = data.results[counter].title;
+            $("UL#list_movies").append("<li>" + title + "</li>");
+        }
+    }
+});
