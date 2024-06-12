2accb84f5a3116b6f27c72f19ba24ce37ca13ee2
ellalearns <ellalearnscopy@gmail.com>
2024-06-12 03:19:19 +0100

    task 11

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const newElem = '<li>Item</li>';
+  const domToMod = 'UL.my_list';
+
+  $('DIV#add_item').on('click', function () {
+    $(domToMod).append(newElem);
+  })
+
+  $('DIV#remove_item').on('click', function () {
+    $(domToMod).children(':last-child').remove();
+  })
+
+  $('DIV#clear_list').on('click', function () {
+    $(domToMod).empty();
+  })
+  
+});
