a3d38ca8c529036c0acdf06382dbae22d2113de2
ellalearns <ellalearnscopy@gmail.com>
2024-06-22 08:46:39 +0100

    selects only rows with score >= 10

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,8 @@
+-- displays all records with score >= 10 
+-- selects score and name columns
+-- orders by score (top first)
+-- db name is arg in ext command
+SELECT id, `name`
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
