be70f728ddb93317e87beedac240dc148b31eaf2
ellalearns <ellalearnscopy@gmail.com>
2024-06-22 08:34:34 +0100

    create table and add multiple rows

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,27 @@
+-- creates a table in a db and adds multiple rows
+-- db name is arg in ext command
+CREATE TABLE IF NOT EXISTS second_table (
+    `id` INT,
+    `name` VARCHAR(256),
+    `score` INT
+);
+INSERT INTO second_table (
+    `id`, `name`, `score`
+) VALUES (
+    1, "John", 10
+);
+INSERT INTO second_table (
+    `id`, `name`, `score`
+) VALUES (
+    2, "Alex", 3
+);
+INSERT INTO second_table (
+    `id`, `name`, `score`
+) VALUES (
+    3, "Bob", 14
+);
+INSERT INTO second_table (
+    `id`, `name`, `score`
+) VALUES (
+    4, "George", 8
+);
