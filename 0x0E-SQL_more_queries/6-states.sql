72563129c368a16da0a46f1610b9d0030bc541b9
ellalearns <ellalearnscopy@gmail.com>
2024-06-23 17:20:39 +0100

    create table in a db. table cols have several constraints.

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,8 +2,7 @@
 -- id col has unique values, is auto generated, can't be null, and is a primary key
 -- name col can't be null
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-USE `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `states` (
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
     `id` INT UNIQUE AUTO_INCREMENT DEFAULT 1,
     `name` VARCHAR(256) NOT NULL
 );
