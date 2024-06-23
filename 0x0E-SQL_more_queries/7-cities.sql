6e3dfb4057575c66a83c0d639ac9186952ca2458
ellalearns <ellalearnscopy@gmail.com>
2024-06-23 17:33:56 +0100

    create table with primary and foreign keys

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- creates a database and table
+-- table contains columns with constraints
+-- primary key and foreign key are declared
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+    PRIMARY KEY(`id`),
+    `id` INT NOT NULL UNIQUE AUTO_INCREMENT DEFAULT 1,
+    `state_id` INT NOT NULL,
+    FOREIGN KEY(`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`),
+    `name` VARCHAR(256) NOT NULL
+);
