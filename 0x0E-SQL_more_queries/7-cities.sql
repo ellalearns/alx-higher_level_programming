da682cafe0adbc4725a12feef8833bd78a2f4306
ellalearns <ellalearnscopy@gmail.com>
2024-06-23 17:45:02 +0100

    create table with primary and foreign keys

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,7 +4,7 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
     PRIMARY KEY(`id`),
-    `id` INT NOT NULL UNIQUE AUTO_INCREMENT DEFAULT 1,
+    `id` INT NOT NULL UNIQUE AUTO_INCREMENT,
     `state_id` INT NOT NULL,
     FOREIGN KEY(`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`),
     `name` VARCHAR(256) NOT NULL
